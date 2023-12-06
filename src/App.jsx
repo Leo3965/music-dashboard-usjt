9ba2a41edd1ca99220cd2beee2f799f41cb30b00
Leo Freitas <leofreitas.engineer@gmail.com>
2023-12-06 19:18:32 -0300

    feat: add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Hero from "./components/Hero";
+import Table from "./components/Table";
 
 function App() {
-
   return (
     <>
-      <section id="hero" className="hero is-large is-info">
-        <div className="hero-body">
-          <p className="title">A3 - Big Data</p>
-          <p className="subtitle">Music Genre Classification</p>
-        </div>
-      </section>
+      <Hero/>
+      <Table/>
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section id="hero" className="hero is-large is-info">
+      <div className="hero-body">
+        <p className="title">A3 - Big Data</p>
+        <p className="subtitle">Music Genre Classification</p>
+      </div>
+    </section>
+  );
+}
